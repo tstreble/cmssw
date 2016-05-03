6655feb1d7559acb0d71a2e1e41ea2687fe04b59
Michael <michaeljmulhearn@gmail.com>
2016-05-04 00:33:53 +0200

    revert writing of new EMTF output formats from EndCap Track Producer

--- L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapTrackProducer.cc
+++ L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapTrackProducer.cc
@@ -31,9 +31,6 @@
 #include "L1Trigger/L1TMuonEndCap/interface/ChargeAssignment.h"
 #include "L1Trigger/L1TMuonEndCap/interface/MakeRegionalCand.h"
 
-// New EDM output for detailed track and hit information - AWB 01.04.16
-#include "DataFormats/L1TMuon/interface/EMTFTrack.h"
-#include "DataFormats/L1TMuon/interface/EMTFHit.h"
 
 using namespace L1TMuon;
 
@@ -43,8 +40,6 @@ L1TMuonEndCapTrackProducer::L1TMuonEndCapTrackProducer(const PSet& p) {
   inputTokenCSC = consumes<CSCCorrelatedLCTDigiCollection>(p.getParameter<edm::InputTag>("CSCInput"));
   
   produces<l1t::RegionalMuonCandBxCollection >("EMTF");
-  produces< l1t::EMTFTrackCollection >("EMTF");
-  produces< l1t::EMTFHitCollection >("EMTF");  
 }
 
 
@@ -58,8 +53,6 @@ void L1TMuonEndCapTrackProducer::produce(edm::Event& ev,
 
   //std::auto_ptr<L1TMuon::InternalTrackCollection> FoundTracks (new L1TMuon::InternalTrackCollection);
   std::auto_ptr<l1t::RegionalMuonCandBxCollection > OutputCands (new l1t::RegionalMuonCandBxCollection);
-  std::auto_ptr<l1t::EMTFTrackCollection> OutputTracks (new l1t::EMTFTrackCollection);
-  std::auto_ptr<l1t::EMTFHitCollection> OutputHits (new l1t::EMTFHitCollection);
 
   std::vector<BTrack> PTracks[NUM_SECTORS];
 
@@ -82,11 +75,11 @@ void L1TMuonEndCapTrackProducer::produce(edm::Event& ev,
     auto dend = (*chamber).second.second;
     for( ; digi != dend; ++digi ) {
       out.push_back(TriggerPrimitive((*chamber).first,*digi));
-      l1t::EMTFHit thisHit;
-      thisHit.ImportCSCDetId( (*chamber).first );
-      thisHit.ImportCSCCorrelatedLCTDigi( *digi );
-      if (thisHit.Station() == 1 && thisHit.Ring() == 1 && thisHit.Strip() > 127) thisHit.set_ring(4);
-      OutputHits->push_back( thisHit );
+      //l1t::EMTFHit thisHit;
+      //thisHit.ImportCSCDetId( (*chamber).first );
+      //thisHit.ImportCSCCorrelatedLCTDigi( *digi );
+      //if (thisHit.Station() == 1 && thisHit.Ring() == 1 && thisHit.Strip() > 127) thisHit.set_ring(4);
+      //OutputHits->push_back( thisHit );
     }
   }
   
@@ -129,54 +122,6 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 
  	std::vector<ConvertedHit> ConvHits = PrimConv(tester,SectIndex);
 	CHits[SectIndex] = ConvHits;
-
-	// Fill OutputHits with ConvertedHit information
-	for (uint iCHit = 0; iCHit < ConvHits.size(); iCHit++) {
-	  // bool isMatched = false;
-	  for (uint iHit = 0; iHit < OutputHits->size(); iHit++) {
-	    if ( ConvHits.at(iCHit).Station() == OutputHits->at(iHit).Station() &&
-		 ( ConvHits.at(iCHit).Id()    == OutputHits->at(iHit).CSC_ID()  ||
-		   ConvHits.at(iCHit).Id()    == ( (OutputHits->at(iHit).Ring() != 4) // Account for either ME1/1a 
-						   ? OutputHits->at(iHit).CSC_ID()    // CSC ID numbering convention
-						   : OutputHits->at(iHit).CSC_ID() + 9 ) ) &&
-		 ConvHits.at(iCHit).Wire()    == OutputHits->at(iHit).Wire()    &&
-		 ConvHits.at(iCHit).Strip()   == OutputHits->at(iHit).Strip()   &&
-		 ConvHits.at(iCHit).BX()      == OutputHits->at(iHit).BX() ) {
-	      // isMatched = true;
-	      OutputHits->at(iHit).set_zone_hit    ( ConvHits.at(iCHit).Zhit()   );
-	      OutputHits->at(iHit).set_phi_hit     ( ConvHits.at(iCHit).Ph_hit() );
-	      OutputHits->at(iHit).set_phi_z_val   ( ConvHits.at(iCHit).Phzvl()  );
-	      OutputHits->at(iHit).set_phi_loc_int ( ConvHits.at(iCHit).Phi()    );
-	      OutputHits->at(iHit).set_theta_int   ( ConvHits.at(iCHit).Theta()  );
-
-	      OutputHits->at(iHit).SetZoneContribution ( ConvHits.at(iCHit).ZoneContribution() );
-	      OutputHits->at(iHit).set_phi_loc_deg  ( GetPackedPhi( OutputHits->at(iHit).Phi_loc_int() ) );
-	      OutputHits->at(iHit).set_phi_loc_rad  ( OutputHits->at(iHit).Phi_loc_deg() * 3.14159/180 );
-	      OutputHits->at(iHit).set_phi_glob_deg ( OutputHits->at(iHit).Phi_loc_deg() + 15 + (OutputHits->at(iHit).Sector() - 1)*60 );
-	      OutputHits->at(iHit).set_phi_glob_rad ( OutputHits->at(iHit).Phi_glob_deg() * 3.14159/180 );
-	      OutputHits->at(iHit).set_theta_deg    ( OutputHits->at(iHit).calc_theta_deg( OutputHits->at(iHit).Theta_int() ) );
-	      OutputHits->at(iHit).set_theta_rad    ( OutputHits->at(iHit).calc_theta_rad( OutputHits->at(iHit).Theta_int() ) );
-	      OutputHits->at(iHit).set_eta( OutputHits->at(iHit).calc_eta( OutputHits->at(iHit).Theta_rad() ) * OutputHits->at(iHit).Endcap() );
-	    }
-	  } // End loop: for (uint iHit = 0; iHit < OutputHits->size(); iHit++)
-
-	  // if (isMatched == false) {
-	  //   std::cout << "***********************************************" << std::endl;
-	  //   std::cout << "Unmatched ConvHit in event " << ev.id().event() << ", SectIndex " << SectIndex << std::endl;
-	  //   std::cout << "ConvHit: station = " << ConvHits.at(iCHit).Station() << ", CSC ID = " << ConvHits.at(iCHit).Id()
-	  // 	      << ", wire = " << ConvHits.at(iCHit).Wire() << ", strip = " << ConvHits.at(iCHit).Strip()
-	  // 	      << ", BX = " << ConvHits.at(iCHit).BX() << std::endl;
-	    
-	  //   for (uint iHit = 0; iHit < OutputHits->size(); iHit++) {
-	  //     std::cout << "EMTFHit: station = " << OutputHits->at(iHit).Station() << ", CSC ID = " << OutputHits->at(iHit).CSC_ID()
-	  // 		<< ", wire = " << OutputHits->at(iHit).Wire() << ", strip = " << OutputHits->at(iHit).Strip()
-	  // 		<< ", BX = " << OutputHits->at(iHit).BX() << ", ring = " << OutputHits->at(iHit).Ring() 
-	  // 		<< ", endcap = " << OutputHits->at(iHit).Endcap() << ", sector = " << OutputHits->at(iHit).Sector() << std::endl;
-	  //   }
-	  // }
-
-	} // End loop: for (uint iCHit = 0; iCHit < ConvHits.size(); iCHit++)
-	
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////print values for input into Alex's emulator code/////////////////////////////////////////////////////
@@ -301,22 +246,6 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 		tempTrack.deltas = AllTracks[fbest].deltas;
 		std::vector<int> ps, ts;
 
-		l1t::EMTFTrack thisTrack;
-		thisTrack.set_phi_loc_int ( AllTracks[fbest].phi           );
-		thisTrack.set_theta_int   ( AllTracks[fbest].theta         );
-		thisTrack.set_rank        ( AllTracks[fbest].winner.Rank() );
-		// thisTrack.set_deltas        ( AllTracks[fbest].deltas        );
-		int tempStraightness = 0;
-		int tempRank = thisTrack.Rank();
-		if (tempRank & 64)
-		  tempStraightness |= 4;
-		if (tempRank & 16)
-		  tempStraightness |= 2;
-		if (tempRank & 4)
-		  tempStraightness |= 1;
-		thisTrack.set_straightness ( tempStraightness );
-
-
 		int sector = -1;
 		bool ME13 = false;
 		int me1address = 0, me2address = 0, CombAddress = 0, mode_uncorr = 0;
@@ -327,22 +256,6 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 
 			if(A->Phi() != -999){
 			  
-			        l1t::EMTFHit thisHit;
-			        // thisHit.ImportCSCDetId( A->TP().detId<CSCDetId>() );
-
-				for (uint iHit = 0; iHit < OutputHits->size(); iHit++) {
-				  if ( A->TP().detId<CSCDetId>().station() == OutputHits->at(iHit).Station() and
-				       A->TP().getCSCData().cscID          == OutputHits->at(iHit).CSC_ID()  and
-				       A->Wire()                           == OutputHits->at(iHit).Wire()    and
-				       A->Strip()                          == OutputHits->at(iHit).Strip()   and
-				       A->TP().getCSCData().bx             == OutputHits->at(iHit).BX() ) {
-				    thisHit = OutputHits->at(iHit);
-				    thisTrack.push_HitIndex(iHit);
-				  }
-				}
-				thisTrack.set_endcap       ( thisHit.Endcap()     );
-				thisTrack.set_sector       ( thisHit.Sector()     );
-				thisTrack.set_sector_GMT   ( thisHit.Sector_GMT() );
 
 				int station = A->TP().detId<CSCDetId>().station();
 				int id = A->TP().getCSCData().cscID;
@@ -435,30 +348,6 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 		float theta_angle = (AllTracks[fbest].theta*0.2851562 + 8.5)*(3.14159265359/180);
 		float eta = (-1)*log(tan(theta_angle/2));
 
-		thisTrack.set_phi_loc_deg  ( (thisTrack.Phi_loc_int() / 60.0) - 2.0 );
-		thisTrack.set_phi_loc_rad  ( thisTrack.Phi_loc_deg() * 3.14159/180 );
-		thisTrack.set_phi_glob_deg ( thisTrack.Phi_loc_deg() + 15 + (thisTrack.Sector() - 1)*60 );
-		thisTrack.set_phi_glob_rad ( thisTrack.Phi_glob_deg() * 3.14159/180 );
-		thisTrack.set_quality    ( outCand.hwQual());
-		thisTrack.set_mode       ( mode            ); 
-		thisTrack.set_first_bx   ( ebx             ); 
-		thisTrack.set_second_bx  ( sebx            ); 
-		thisTrack.set_phis       ( ps              );
-		thisTrack.set_thetas     ( ts              );
-		thisTrack.set_pt         ( xmlpt*1.4       );
-		thisTrack.set_pt_XML     ( xmlpt           );
-		thisTrack.set_pt_LUT     ( xmlpt_address   );
-		thisTrack.set_charge     ( (charge == 1) ? 1 : -1 );
-		thisTrack.set_charge_GMT ( charge          );
-		thisTrack.set_theta_rad  ( theta_angle     );
-		thisTrack.set_theta_deg  ( theta_angle * 180/3.14159265359 );
-		thisTrack.set_eta        ( eta  * thisTrack.Endcap() );
-		thisTrack.set_pt_GMT     ( outCand.hwPt()  ); 
-		thisTrack.set_phi_GMT    ( outCand.hwPhi() );
-		thisTrack.set_eta_GMT    ( outCand.hwEta() );
-
-		thisTrack.Import_pT_LUT  ( thisTrack.Mode(), thisTrack.Pt_LUT() );
-
 		// thisTrack.phi_loc_rad(); // Need to implement - AWB 04.04.16
 		// thisTrack.phi_glob_rad(); // Need to implement - AWB 04.04.16
 
@@ -481,9 +370,7 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 		  // 	    << ", processor = " << outCand.processor() 
 		  // 	    << ", trackFinderType = " << outCand.trackFinderType() << std::endl;
 			holder.push_back(outPair);
-			thisTrack.set_isGMT( 1 );
 		}
-		OutputTracks->push_back( thisTrack );
 	}
   }
   
@@ -508,9 +395,6 @@ for(int sect=0;sect<12;sect++){
 
 //ev.put( FoundTracks, "DataITC");
 ev.put( OutputCands, "EMTF");
- ev.put( OutputHits, "EMTF"); 
- ev.put( OutputTracks, "EMTF");
-  //std::cout<<"End Upgraded Track Finder Prducer:::::::::::::::::::::::::::\n:::::::::::::::::::::::::::::::::::::::::::::::::\n\n";
 
 }//analyzer
 
